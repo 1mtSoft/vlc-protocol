97a0d805fd73ec1c3f5f1d99c981334b7c0ccfdf
Maxim Nikitin <nikitin@byndyusoft.com>
2023-08-18 13:36:52 +0500

    Added: download *.vlm

--- vlc-protocol.bat
+++ vlc-protocol.bat
@@ -1,6 +1,9 @@
 @echo off
 Setlocal EnableDelayedExpansion
 set command=%~1
-set url=%~2
-set url=!url: =%%20!
-start "VLC" "%~dp0vlc.exe" %command% "%url:~6%"
+set url_full=%~2
+set url=%url_full:~6%
+set file_name=%~nx2
+set file=%tmp%\%file_name%
+powershell -Command "(New-Object Net.WebClient).DownloadFile(\"%url%\", \"%file%\")"
+start "VLC" "%~dp0vlc.exe" %command% %file%
