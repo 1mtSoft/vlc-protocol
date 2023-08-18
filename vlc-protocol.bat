2fe84657203e05285b2e2c373aff99fbab2a9bd2
Maxim Nikitin <nikitin@byndyusoft.com>
2023-08-18 13:38:44 +0500

    Fixed: download *.vlm

--- vlc-protocol.bat
+++ vlc-protocol.bat
@@ -4,6 +4,6 @@ set command=%~1
 set url_full=%~2
 set url=%url_full:~6%
 set file_name=%~nx2
-set file=%tmp%\%file_name%
+set file=%tmp%/%file_name%
 powershell -Command "(New-Object Net.WebClient).DownloadFile(\"%url%\", \"%file%\")"
 start "VLC" "%~dp0vlc.exe" %command% %file%
