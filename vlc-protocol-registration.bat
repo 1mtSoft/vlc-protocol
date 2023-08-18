30784f960a138c8f87b567363b9611a7008050fe
Maxim Nikitin <nikitin@byndyusoft.com>
2023-08-18 15:32:25 +0500

    Fixed: download *.vlm

--- vlc-protocol-registration.bat
+++ vlc-protocol-registration.bat
@@ -27,9 +27,14 @@ reg delete HKCR\vlc /f
 echo Removing vlm:// association.
 reg delete HKCR\vlm /f
 
-if exist "%vlc_dir:"=%\vlc-protocol.bat" (
-  echo Deleting vlc-protocol.bat
-  del /F /Q "%vlc_dir:"=%\vlc-protocol.bat"
+if exist "%vlc_dir:"=%\vlc-protocol-rtsp.bat" (
+  echo Deleting vlc-protocol-rtsp.bat
+  del /F /Q "%vlc_dir:"=%\vlc-protocol-rtsp.bat"
+)
+
+if exist "%vlc_dir:"=%\vlc-protocol-vlm.bat" (
+  echo Deleting vlc-protocol-vlm.bat
+  del /F /Q "%vlc_dir:"=%\vlc-protocol-vlm.bat"
 )
 
 if exist "%vlc_dir:"=%\bg_1x1_1920x1080.png" (
@@ -57,8 +62,11 @@ if exist "%vlc_dir:"=%\bg_3x3_1920x1080.png" (
   del /F /Q "%vlc_dir:"=%\bg_3x3_1920x1080.png"
 )
 
-echo Copy vlc-protocol.bat to %vlc_dir%
-copy /Y "%~dp0vlc-protocol.bat" "%vlc_dir:"=%\vlc-protocol.bat"
+echo Copy vlc-protocol-rtsp.bat to %vlc_dir%
+copy /Y "%~dp0vlc-protocol-rtsp.bat" "%vlc_dir:"=%\vlc-protocol-rtsp.bat"
+
+echo Copy vlc-protocol-vlm.bat to %vlc_dir%
+copy /Y "%~dp0vlc-protocol-vlm.bat" "%vlc_dir:"=%\vlc-protocol-vlm.bat"
 
 echo Copy bg_1x1_1920x1080.png to %vlc_dir%
 copy /Y "%~dp0bg_1x1_1920x1080.png" "%vlc_dir:"=%\bg_1x1_1920x1080.png"
@@ -79,13 +87,13 @@ echo Associating vlc:// with vlc-protocol.bat.
 reg add HKCR\vlc /ve /t REG_SZ /d "URL:vlc Protocol" /f
 reg add HKCR\vlc /v "URL Protocol" /t REG_SZ /d "" /f
 reg add HKCR\vlc\DefaultIcon /ve /t REG_SZ /d "%vlc_dir:"=%\vlc.exe,0" /f
-reg add HKCR\vlc\shell\open\command /ve /t REG_SZ /d "\"%vlc_dir:"=%\vlc-protocol.bat\" --open \"%%1\"" /f
+reg add HKCR\vlc\shell\open\command /ve /t REG_SZ /d "\"%vlc_dir:"=%\vlc-protocol-rtsp.bat\" \"%%1\"" /f
 
 echo Associating vlm:// with vlc-protocol.bat.
 reg add HKCR\vlm /ve /t REG_SZ /d "URL:vlm Protocol" /f
 reg add HKCR\vlm /v "URL Protocol" /t REG_SZ /d "" /f
 reg add HKCR\vlm\DefaultIcon /ve /t REG_SZ /d "%vlc_dir:"=%\vlc.exe,0" /f
-reg add HKCR\vlm\shell\open\command /ve /t REG_SZ /d "\"%vlc_dir:"=%\vlc-protocol.bat\" --vlm-conf \"%%1\"" /f
+reg add HKCR\vlm\shell\open\command /ve /t REG_SZ /d "\"%vlc_dir:"=%\vlc-protocol-vlm.bat\" \"%%1\"" /f
 
 @echo.
 pause
--- vlc-protocol-vlm.bat
+++ vlc-protocol-vlm.bat
@@ -1,9 +1,10 @@
 @echo off
 Setlocal EnableDelayedExpansion
-set command=%~1
-set url_full=%~2
+set url_full=%~1
 set url=%url_full:~6%
-set file_name=%~nx2
+set file_name=%~nx1
 set file=%tmp%\%file_name%
+
+cd "%~dp0"
 powershell -Command "(New-Object Net.WebClient).DownloadFile(\"%url%\", \"%file%\")"
-start "VLC" "%~dp0vlc.exe" %command% %file%
+start "VLC" "%~dp0vlc.exe" --vlm-conf %file%
